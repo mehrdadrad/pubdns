7d306ebba3bcf4757c8614bd213d6e043f4c1804
Mehrdad Arshad Rad <arshad.rad@gmail.com>
2017-12-14 18:11:45 -0800

    diff changes

--- pubdns/pubdns.py
+++ pubdns/pubdns.py
@@ -39,30 +39,45 @@ class PubDNS(object):
             if len(fields) < 4 or fields[m['city']] == '':
                 continue
             rec = dict(city=fields[m['city']], server=fields[m['ip']],
-                       reliability=fields[m['reliability']])
+                       name=fields[m['name']], reliability=fields[m['reliability']])
             self.data[fields[m['country_id']]].append(rec)
 
     def _load_data(self):
-        filename = os.path.join(self.home, '.publicdns.csv')
+        filename = os.path.join(self.home, '.publicdns')
         with open(filename, 'r') as f:
             PubDNS.data = json.load(f)
 
     def _save_data(self):
-        filename = os.path.join(self.home, '.publicdns.csv')
+        filename = os.path.join(self.home, '.publicdns')
         with open(filename, 'w') as f:
             f.write(json.dumps(PubDNS.data))
 
-    def get_servers(self, country_id, city=''):
+    def xservers(self, country_id, city=''):
         """ Return servers based on the country / city """
 
         if country_id not in PubDNS.data:
             return {}
 
-        if city != '':
+        city = city.lower()
+        for rec in PubDNS.data[country_id]:
+            if city == '' or rec['city'].lower() == city:
+                yield rec
+
+    def servers(self, country_id, city=''):
+        """ Return servers based on the country / city """
+
+        if country_id not in PubDNS.data:
+            return {}
+
+        city = city.lower()
+        if city == '':
+            return PubDNS.data[country_id]
+        else:
+            recs = []
             for rec in PubDNS.data[country_id]:
-                if rec['city'] == city:
-                    yield rec
-        return (x for x in PubDNS.data[country_id])
+                if city == '' or rec['city'].lower() == city:
+                    recs.append(rec)
+            return recs       
 
     def update(self):
         """ Fetch and save pub dns info """
@@ -73,6 +88,7 @@ class PubDNS(object):
             self._save_data()
         except Exception as e:
             logging.debug(e)
+            raise 'Can not fetch / update public dns'
 
 def pubdns():
     """ Return a :class:`PubDNS` """
