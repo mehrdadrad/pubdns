0b11838fce6b6cb5a1434213e8ad3f7177b69679
Mehrdad Arshad Rad <arshad.rad@gmail.com>
2017-12-14 18:12:21 -0800

    add dns

--- pubdns/__init__.py
+++ pubdns/__init__.py
@@ -1,6 +1,7 @@
 from pubdns.pubdns import pubdns
+from pubdns.dns import resolver
 
 __author__ = 'Mehrdad Arshad Rad (arshad.rad@gmail.com)'
 __license__ = 'MIT'
 __version__ = 'dev'
-__all__ = ['pubdns']
+__all__ = ['pubdns', 'dns']
