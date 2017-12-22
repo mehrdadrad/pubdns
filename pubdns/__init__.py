4c835e3fc8d1e426bc103304c72983ab4e6d416b
Mehrdad Arshad Rad <arshad.rad@gmail.com>
2017-12-21 17:20:02 -0800

    add comment

--- pubdns/__init__.py
+++ pubdns/__init__.py
@@ -1,3 +1,23 @@
+"""
+PubDNS is a python library to interact with public DNS servers around the world
+
+Usage:
+    >>> import pubdns
+    >>> pd = pubdns.pubdns()
+    >>> servers = pd.servers('US')
+    >>> print(len(servers))
+    2714
+    >>> print(servers.pop())
+    {
+        'city': 'San Francisco',
+        'server': '162.243.136.199',
+        'name': '',
+        'reliability': '1.00'
+    }
+
+:license: MIT
+"""
+
 from .pubdns import pubdns, PubDNS
 from .dns import resolver
 from .exceptions import UpdateError
